7a44133083c6916154ff9ca55f56e7ece9904429
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-29 12:36:24 -0400

    completed with remove button only

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const taskPriority = document.createElement('select')
-  const priorityHigh = document.createElement('option')
-  const priorityMedium = document.createElement('option')
-  const priorityLow = document.createElement('option')
-  priorityHigh.innerText = 'High Priority'
-  priorityMedium.innerText = 'Medium Priority'
-  priorityLow.innerText = 'Low Priority'
-  taskPriority.id = 'priority'
-  const tPriority = document.getElementById('priority')
-  document.querySelector('#create-task-form').append(taskPriority)
-  taskPriority.append(priorityHigh, priorityMedium, priorityLow)
+  // const taskPriority = document.createElement('select')
+  // const priorityHigh = document.createElement('option')
+  // const priorityMedium = document.createElement('option')
+  // const priorityLow = document.createElement('option')
+  // priorityHigh.innerText = 'High Priority'
+  // priorityMedium.innerText = 'Medium Priority'
+  // priorityLow.innerText = 'Low Priority'
+  // taskPriority.id = 'priority'
+  // const tPriority = document.getElementById('priority')
+  // document.querySelector('#create-task-form').append(taskPriority)
+  // taskPriority.append(priorityHigh, priorityMedium, priorityLow)
   document.querySelector("#create-task-form").addEventListener("submit", (event) =>{
     event.preventDefault()
 
@@ -17,22 +17,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const tasks = document.querySelector("#tasks")
     const element = document.createElement("li")
     element.innerText = newTask
+    element.className = 'task'
     tasks.append(element)
     const button = document.createElement('button')
+    button.id = 'btn'
     button.innerText = "X"  
     button.addEventListener('click', () => element.remove())
     element.append(button)
 
-    if (tPriority.selectedIndex === 0){
-      element.style.color = 'red'
-
-    } else if (tPriority.selectedIndex === 1){
-      element.style.color = 'yellow'
-
-    } else if (tPriority.selectedIndex === 2) {
-      element.style.color = 'green'
-
-    }
   })
-
 })
